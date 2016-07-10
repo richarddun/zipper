73f949577536776d8c53ac794e24f8d0e5c251f1
richarddun <richard.w.dunne@gmail.com>
2016-07-10 22:37:12 +0100

    modifying 1 px buffer for left / right collision to prevent sticking on corners

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from kivy.core.window import Window, Keyboard
 from kivy.uix.image import Image
 from kivy.atlas import Atlas
 from kivy.uix.widget import Widget
-#from kivy.metrics import sp
 from kivy.clock import Clock
 import tmx
 from rect import Rect
@@ -37,23 +36,26 @@ class params(object):
 
 class Player_Sprite(Image):
     def __init__(self,pos,mapz,**kwargs):
-        super(Player_Sprite,self).__init__(pos=pos,**kwargs)
-        self.images = Atlas('animation/animatlas/zipperanimatlas.atlas')
+        super(Player_Sprite,self).__init__(pos=pos,**kwargs)  #size=(145,80),
+        self.mov_images = Atlas('animation/dup/dupdup/zipperanimatlas.atlas')
         self.map = mapz
-        self.texture = self.images['walk_1_right']
-        self.rightick = 0
-        self.leftick = 0
+        self.texture = self.mov_images['walk_1_right']
         self.moving_right = False
         self.resting = True
         self.moving_left = False
-        self.maxjump = 10 * params.scale
         self.movyval = 0
+        self.suspended = 0
         self.jumping = False
+
 
     def update(self, *ignore):
         dx,self.dy = 0,0
-        last = Rect(*(self.pos + self.size))
+        last = Rect(*(self.pos + self.size))#Rect(self.pos[0]+(self.width*.25),self.pos[1], (self.size[0]*.47), self.size[1])
         if keys.get(Keyboard.keycodes['spacebar']) and self.resting:
+            if self.moving_right:
+                self.texture = self.mov_images['walk_2_right']
+            elif self.moving_left:
+                self.texture = self.mov_images['walk_2_left']
             self.jumping = True
             self.resting = False
             self.dy = 5 * params.scale
@@ -62,31 +64,29 @@ class Player_Sprite(Image):
                 self.jumping = False
                 self.movyval = 0
                 if self.moving_right:
-                    self.texture = self.images['walk_2_right']
+                    self.texture = self.mov_images['walk_2_right']
                 elif self.moving_left:
-                    self.texture = self.images['walk_2_left']
+                    self.texture = self.mov_images['walk_2_left']
                 self.dy -= 6 * params.scale
             else:
                 if self.moving_right:
-                    self.texture = self.images['jump_r']
+                    self.texture = self.mov_images['jump_r']
                 elif self.moving_left:
-                    self.texture = self.images['jump_l']
+                    self.texture = self.mov_images['jump_l']
                 self.dy = 5 * params.scale
                 self.movyval += .5
-        elif self.jumping and not keys.get(Keyboard.keycodes['spacebar']):
+        if self.jumping and not keys.get(Keyboard.keycodes['spacebar']):
             self.jumping = False
             self.movyval = 0
-        elif self.jumping == False and self.y > 0:
+        if not self.jumping:
             if self.moving_right:
-                self.texture = self.images['walk_2_right']
+                self.texture = self.mov_images['walk_2_right']
                 self.dy -= 6 * params.scale
             elif self.moving_left:
-                self.texture = self.images['walk_2_left']
+                self.texture = self.mov_images['walk_2_left']
                 self.dy -= 6 * params.scale
             else:
                 self.dy -= 6 * params.scale
-        elif self.jumping == False and self.y <= 0:
-            self.resting = True
         if keys.get(Keyboard.keycodes['left']) and not keys.get(Keyboard.keycodes['right']):
             dx -= 5 * params.scale
             self.moving_left = True
@@ -98,32 +98,28 @@ class Player_Sprite(Image):
         elif (keys.get(Keyboard.keycodes['right']) and (keys.get(Keyboard.keycodes['left']))):
             self.moving_left, self.moving_right = False,False
         elif not self.jumping and not (keys.get(Keyboard.keycodes['right']) and (keys.get(Keyboard.keycodes['left']))):
-            if self.moving_left:
-                self.texture = self.images['walk_1_left']
+            if self.moving_left and self.resting:
+                self.texture = self.mov_images['walk_1_left']
                 self.moving_left = False
-            elif self.moving_right:
-                self.texture = self.images['walk_1_right']
+            elif self.moving_right and self.resting:
+                self.texture = self.mov_images['walk_1_right']
                 self.moving_right = False
         if self.moving_left:
             if self.jumping:
-                self.texture = self.images['jump_l']
+                self.texture = self.mov_images['jump_l']
             else:
-                self.texture = self.images['walk_2_left']
+                self.texture = self.mov_images['walk_2_left']
         if self.moving_right:
             if self.jumping:
-                self.texture = self.images['jump_r']
+                self.texture = self.mov_images['jump_r']
             else:
-                self.texture = self.images['walk_2_right']
+                self.texture = self.mov_images['walk_2_right']
         self.x += dx
         self.y += self.dy
         new = Rect(*(self.pos + self.size))
-
+        #new=Rect(self.pos[0]+(self.width*.25),self.pos[1], (self.size[0]*.47), self.size[1])
         for cell in self.map.map.layers['blocker'].collide(new, 'blocker'):
             blocker = cell['blocker']
-            if 'l' in blocker and last.right <= cell.left and new.right > cell.left:
-                new.right = cell.left
-            if 'r' in blocker and last.left >= cell.right and new.left < cell.right:
-                new.left = cell.right
             if 't' in blocker and last.bottom >= cell.top and new.bottom < cell.top:
                 self.resting = True
                 new.bottom = cell.top
@@ -131,7 +127,13 @@ class Player_Sprite(Image):
             if 'b' in blocker and last.top <= cell.bottom and new.top > cell.bottom:
                 new.top = cell.bottom
                 self.dy = 0
-        self.pos = new.bottomleft
+                self.suspended = 0
+            if 'l' in blocker and last.right <= cell.left and new.right > cell.left:
+                new.right = cell.left-1
+            if 'r' in blocker and last.left >= cell.right and new.left < cell.right:
+                new.left = cell.right+1
+
+        self.pos = new.bottomleft#[0]-self.width*.25, new.bottomleft[1]
 
 params = params()
 
